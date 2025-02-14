e3465a1dfae15e4fc5887e5711db9988c84f36a4
geniseb <genise.butler@outlook.com>
2025-02-14 11:28:39 -0700

    added imaage to page

--- portfolio/src/Views/Pages/About.jsx
+++ portfolio/src/Views/Pages/About.jsx
@@ -1,17 +1,25 @@
 import React from "react";
+import Image from "../../Assets/Images/aboutimage.png"
 
 
 const About = () => {
     return (
         <>
-        <div className="aboutheader">
-            <h1>About Me</h1>
-        </div>
+            
+           
+            <div className="about-header">
+                <h1>About Me</h1>
+            </div>
+            
+            <div className="about-container">
+            <div className="about-text">
+                <p>Highly skilled and innovative professional, proficient in graphic design, seeking a dynamic career opportunity that leverages my extensive design and computer experience. I am a passionate and adaptable problem-solver, committed to continuous learning, and possess strengths in leadership, teamwork, and creativity.</p>
+            </div>
 
-        <div className="abouttext">
-            <p abouttext>Highly skilled and innovative professional, proficient in graphic design, seeking a dynamic career opportunity that leverages my extensive design and computer experience. I am a passionate and adaptable problem-solver, committed to continuous learning, and possess strengths in leadership, teamwork, and creativity.</p>
-        </div>
+            <img src={Image} alt="picture of graphic technology" className="about-image" />
+            </div>
         </>
-    )
+    );
 }
+
 export default About;
