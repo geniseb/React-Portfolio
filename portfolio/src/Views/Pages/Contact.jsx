5b54d7914dac3c867abf93e10b912292697a14f6
geniseb <genise.butler@outlook.com>
2025-02-24 09:59:25 -0700

    added title to page

--- portfolio/src/Views/Pages/Contact.jsx
+++ portfolio/src/Views/Pages/Contact.jsx
@@ -10,20 +10,20 @@ const Contact = () => {
                 <img src={Image} alt="background picture" className="contact-image" />
             </div>
 
-            {/* <div className="header-container">
+            <div className="header-container">
                 <h1 className="contact-title">Genise <br />Butler</h1>
 
-            </div> */}
-{/* 
+            </div>
+
             <div className="contact-section2">
-                <h5 className="contact-section2">(602) 366-5873</h5>
+                {/* <h5 className="contact-section2">(602) 366-5873</h5>
               <a href="#"><h5 className="contact-section2">genise.butler@outlook.com</h5></a>
                 <h5 className="contact-section2">4032 E. Shangri-La Road</h5>
-                <h5 className="contact-section2">Phoenix, AZ 85028</h5>
+                <h5 className="contact-section2">Phoenix, AZ 85028</h5> */}
 
             </div >
 
-            <form className="contact-form"></form> */}
+            {/* <form className="contact-form"></form> */}
 
             
         </div>
